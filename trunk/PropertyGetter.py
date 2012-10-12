94bb764aa925c4d12d626be784ff5173849b3697
jf@jf.cz <jf@jf.cz@079eff7a-f8b8-e76e-970f-52125a00d28b>
2012-10-12 20:22:11 +0000

    + new control entities

---
+++ trunk/PropertyGetter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import re
+
+
+class Property:
+    def __init__(self, propSpec):
+        if not re.match(r"^[etosa]\.\w+", propSpec):
+            raise RuntimeError("invalid property specifier")
+        self.locator, self.propName = propSpec.split(".")
+        
+    def get(self, transaction, entity):
+        obj = None
+        if self.locator == "a":
+            obj = transaction.actor
+            if(hasattr(obj, self.propName)):
+                return getattr(obj, self.propName)
+            else:
+                return obj.props[self.propName]
+        if self.locator == "t":
+            obj = transaction
+        elif self.locator == "s":
+            obj = transaction.simulation
+        elif self.locator == "e":
+            obj = entity
+        elif self.locator == "o":
+            obj = entity.sharedObject
+        return getattr(obj, self.propName)
