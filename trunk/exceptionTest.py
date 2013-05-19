9a34f22bbcc054e6bcea54f524cf9512f0e8fc9b
jf@jf.cz <jf@jf.cz@079eff7a-f8b8-e76e-970f-52125a00d28b>
2013-05-19 18:30:51 +0000

    + simplification of nested entities

--- trunk/exceptionTest.py
+++ trunk/exceptionTest.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
 from Etos import *
+import Pause
+
+registerModule(Pause)
 
 sim = Simulation()
 #sim.disableLog()
 sim.start("""
 <transaction>
     <try_catch exception="z">
-        <transaction>
-            <exception type="z"/>
-        </transaction>
-        <transaction>
-            <counted_loop>
-                <count>3</count>
-                <transaction>
-                    <trace text="exception"/>
-                </transaction>
-            </counted_loop>    
-        </transaction>
+        <exception type="z"/>
+        <counted_loop restart="crash">
+                <count>5</count>
+                <pause><duration>0:01</duration></pause>
+                <trace text="pass"/>
+                <with>
+                        <probability>0.5</probability>
+                        <exception type="crash"/>
+                </with>
+                <trace text="post-exception"/>
+        </counted_loop>
     </try_catch>
     <trace text="exit"/>
 </transaction>
