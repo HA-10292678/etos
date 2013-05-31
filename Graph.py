ed53db201ab36c338254a0593b6768072ac4480d
jamaic230 <jamaic230@079eff7a-f8b8-e76e-970f-52125a00d28b>
2013-05-31 07:33:13 +0000

    + finding shortest path

--- Graph.py
+++ Graph.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import subprocess
+
 def toFunction(value):
     return value if callable(value) else lambda t=None: value #per key closure
 
@@ -28,6 +30,29 @@ class SGDEntity:
     def get(self, key, default):
         return self.attrs.get(key, default)
 
+class SGDDistance:
+    def __init__(self, distance, previous):
+        self.distance = distance
+        self.previous = previous
+
+class SGDPath:
+    def __init__(self, startNode, finalNode, distance, path):
+        self.startNode = startNode
+        self.finalNode = finalNode
+        self.distance = distance
+        self.path = path
+
+    def __str__(self):
+        pathlist = []
+        pathlist.append(str(self.distance))
+        pathlist.append(" = ")
+        for node in self.path:
+            pathlist.append(node.id())
+            pathlist.append(" -> ")
+        pathlist.append(self.startNode.id())
+        pathlist.reverse()
+        return "".join(pathlist)
+
 class SGDNode(SGDEntity):
     counter = 0
     def __init__(self, **kwargs):
@@ -47,18 +72,29 @@ class SGDNode(SGDEntity):
         return (edge, backEdge)   #returns pair of edges       
     
     def __str__(self):
-        return "Node:{0}".format(self.attrs["id"]())
+        attrlist = []
+        for key, value in self.attrs.items():
+            attrlist.append(key)
+            attrlist.append(": ")
+            attrlist.append(str(value()))
+            attrlist.append("\\n")
+        return "{0} [label=\"{1}\"]".format(self.id(),  "".join(attrlist))
 
 class SGDEdge(SGDEntity):
     counter = 0
-    
     def __init__(self, start, target, **kwargs):
         super().__init__(**kwargs)
         self.start = start
         self.target = target
 
     def __str__(self):
-        return "{0}->{1}".format(self.start.id(), self.target.id())
+        attrlist = []
+        for key, value in self.attrs.items():
+            attrlist.append(key)
+            attrlist.append(": ")
+            attrlist.append(str(value()))
+            attrlist.append("\\n")
+        return "{0} -> {1} [label=\"{2}\"]".format(self.start.id(), self.target.id(), "".join(attrlist))
 
 class DiGraph:
     def __init__(self, *args):
@@ -72,13 +108,103 @@ class DiGraph:
         
     def __len__(self):
         return len(self.nodes)
+    
+    def getDistances(self, startNode, attr):
+        distances = {}
+        queue = []
+        
+        for node in self.nodes:
+            if (node != startNode):
+                distances[node] = SGDDistance(float('inf'), None)
+            else:
+                distances[node] = SGDDistance(0, None)
+            queue.append(node)
+        
+        while (len(queue) > 0):
+            minimum = distances[min(queue, key=lambda node: distances[node].distance)].distance
+            if (minimum == float('inf')):
+                break # discontinuous graph
+            for node in [node for node in queue if distances[node].distance == minimum]:
+                queue.remove(node)
+                for edge in node.edges:
+                    # TODO: capacity of edges
+                    alt = distances[edge.start].distance + edge.attrs[attr]() + edge.start.attrs[attr]()
+                    if (distances[edge.target].distance > alt):
+                        distances[edge.target] = SGDDistance(alt, node)
+        
+        return distances
+    
+    def processDistance(self, distances, finalNode):
+        path = []
+        temp = finalNode
+        while (temp != startNode):
+            if (temp == None):
+                break # path does not exist
+            path.append(temp)
+            temp = distances[temp].previous
+        return path
+    
+    def getPaths(self, startNode, attr, nodeFilter=lambda node: True):
+        distances = self.getDistances(startNode, attr)
+        paths = []
+        for finalNode in distances.keys():
+            if nodeFilter(finalNode):
+                paths.append(SGDPath(startNode, finalNode, distances[finalNode].distance, self.processDistance(distances, finalNode)))
+        return paths
+    
+    def toDot(self):
+        code = []
+        code.append("digraph {\n")
+        for node in self.nodes:
+            code.append("    ")
+            code.append(str(node))
+            code.append("\n")
+        for node in self.nodes:
+            for edge in node.edges:
+                code.append("    ")
+                code.append(str(edge))
+                code.append("\n")
+        code.append("}\n")
+        return ''.join(code)
 
-n1 = SGDNode(id="mesto", delay=10, fuel=True)
-n2 = SGDNode(delay=20, fuel=True)
+nA = SGDNode(id="A", delay=0, final=0)
+nB = SGDNode(id="B", delay=0, final=0)
+nC = SGDNode(id="C", delay=0, final=0)
+nD = SGDNode(id="D", delay=0, final=0)
+nE = SGDNode(id="E", delay=0, final=1)
+nF = SGDNode(id="F", delay=0, final=1)
+nG = SGDNode(id="G", delay=0, final=1)
+nX = SGDNode(id="X", delay=0, final=0)
 
-graph = DiGraph(n1, n2)
+graph = DiGraph(nA, nB, nC, nD, nE, nF, nG, nX)
 
-print( n1.edgeTo(n2, delay=50) )
-print(n1.delay())
+nA.edgeTo(nB, delay=1)
+nA.edgeTo(nC, delay=3)
+nA.edgeTo(nF, delay=8)
+nB.edgeTo(nD, delay=4)
+nB.edgeTo(nF, delay=9)
+nC.edgeTo(nE, delay=1)
+nC.edgeTo(nF, delay=2)
+nD.edgeTo(nG, delay=2)
+nD.edgeTo(nF, delay=5)
+nE.bidiEdgesTo(nF, delay=1)
+nG.bidiEdgesTo(nF, delay=1)
 
-print (",".join(str(node) for node in graph.ifTrueFilter("fuel")))
+# z A do A je 0
+# z A do B je 1
+# z A do C je 3
+# z A do D je 5
+# z A do E je 4
+# z A do F je 5
+# z A do G je 6
+
+print(graph.toDot())
+
+startNode = nA
+for path in graph.getPaths(startNode, "delay", lambda node: node.final()):
+    print(str(path))
+print()
+for path in graph.getPaths(startNode, "delay"):
+    print(str(path))
+
+
